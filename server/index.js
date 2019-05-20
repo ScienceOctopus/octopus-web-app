477c17530ebd7fbcc18fd848478d07bdc69cb67c
Alexander Harkness <me@bearbin.net>
2019-05-20 16:43:39 +0100

    Environment now generates port for backend server to use

--- server/index.js
+++ server/index.js
@@ -1,31 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./queries');
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./queries");
 
 const app = express();
-const port = 3001;
+const port = process.end.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
-app.get('/', (request, response) => {
-	response.json({ info: 'Node.js, Express, and Postgres API' });
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
 });
 
-app.get('/users', db.getUsers);
-app.get('/users/:id', db.getUserById);
-app.post('/users', db.createUser);
-app.put('/users/:id', db.updateUser);
-app.delete('/users/:id', db.deleteUser);
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
 app.listen(port, () => {
-	console.log(`App running on port ${port}.`);
+  console.log(`App running on port ${port}.`);
 });
-
 
 //const pino = require('express-pino-logger')();
 //app.use(bodyParser.urlencoded({ extended: false }));
