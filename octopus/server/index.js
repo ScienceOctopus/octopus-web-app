4b2ad56e57c302654d1ea030da0cde84601afd13
Moritz Langenstein <ml5717@ic.ac.uk>
2019-05-20 15:37:57 +0100

    (ml5717) (im2517) Simple PostgreSQL DB connection

--- octopus/server/index.js
+++ octopus/server/index.js
@@ -1,17 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
+const db = require('./queries');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(pino);
+const port = 3001;
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+
+app.get('/', (request, response) => {
+	response.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
-);
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+
+app.listen(port, () => {
+	console.log(`App running on port ${port}.`);
+});
+
+
+//const pino = require('express-pino-logger')();
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(pino);
+
+//app.get('/api/greeting', (req, res) => {
+//  const name = req.query.name || 'World';
+//  res.setHeader('Content-Type', 'application/json');
+//  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+//});
+
+//app.listen(3001, () =>
+//  console.log('Express server is running on localhost:3001')
+//);
---
+++ octopus/server/queries.js
@@ -0,0 +1,74 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+	user: 'ml5717',
+	host: 'localhost',
+	database: 'api',
+	password: 'password',
+	port: 5432,
+});
+
+const getUsers = (request, response) => {
+	pool.query('SELECT * FROM users ORDER BY id ASC', (error, result) => {
+		if (error) {
+			throw error;
+		}
+		response.status(200).json(result.rows);
+	});
+};
+
+const getUserById = (request, response) => {
+	const id = parseInt(request.params.id)
+
+	pool.query('SELECT * FROM users WHERE id = $1', [id], (error, result) => {
+		if (error) {
+			throw error;
+		}
+		response.status(200).json(result.rows);
+	});
+}
+
+const createUser = (request, response) => {
+  const { name, email } = request.body
+
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id', [name, email], (error, result) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${result.rows[0].id}`)
+  })
+}
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, result) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, result) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+}
