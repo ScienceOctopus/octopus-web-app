493881b74f59c2871f182d224e4870a3c7c3978c
Moritz Langenstein <ml5717@ic.ac.uk>
2019-05-28 19:17:36 +0100

    (ml5717) Backend slack feedback integration

--- server/feedback.js
+++ server/feedback.js
@@ -1,10 +1,28 @@
+const axios = require("axios");
+
 const VALID_MIMETYPES = ["image/jpg", "image/jpeg", "image/png", "image/gif"];
 
 const postFeedback = (req, res) => {
-  console.log(req.body);
-  return res.status(200).send({
-    status: 200,
-    statusText: "Hello World!",
+  let message = req.body.attachments[0];
+
+  delete message.author_name;
+
+  message.title =
+    message.title.charAt(0).toUpperCase() + message.title.slice(1);
+  delete message.title_link;
+
+  if (message.image_url) {
+    message.image_url =
+      "http://octopus-publishing.azurewebsites.net" + message.image_url;
+  }
+
+  message.footer = "Octopus Slack Feedback";
+
+  axios.post(process.env.FEEDBACK_WEBHOOK, req.body).then(response => {
+    return res.status(response.status).send({
+      status: response.status,
+      statusText: response.statusText,
+    });
   });
 };
 
