0bcf73c94c97a5c87c52e875588df5432fc4e319
Tiger Wang <ziwei.tiger@outlook.com>
2019-06-03 16:41:34 +0100

    Preparatory migration for ORCiD integration

---
+++ migrations/1559573402236_orcid-integration.js
@@ -0,0 +1,18 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+	pgm.dropColumns("users", ["password", "login_type"]);
+	pgm.renameColumn("users", "orcid_key", "orcid");
+};
+
+exports.down = (pgm) => {
+	pgm.addColumns("user", {
+		login_type: {
+		  type: "text",
+		},
+		password: {
+		  type: "text",
+		},
+	});
+	pgm.renameColumn("users", "orcid", "orcid_key");
+};
