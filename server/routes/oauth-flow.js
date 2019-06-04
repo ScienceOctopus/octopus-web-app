e52224271171c682e2365173d4b0fc921bdd5a9b
Tiger Wang <ziwei.tiger@outlook.com>
2019-06-04 15:04:49 +0100

    Query database for user during OAuth login
    + Looks for user when OAuth completes
    + Will now add a user when not found
    + Redirects to front-end with user id or error code

--- server/routes/oauth-flow.js
+++ server/routes/oauth-flow.js
@@ -3,15 +3,31 @@ const request = require('request');
 
 const db = require("../postgresQueries.js").queries;
 
-const handleOAuthAuthenticationResponse = (req, res) => {
+const GOBLINID_EXCHANGE_OAUTH_TOKEN_ADDRESS = 'https://orcid.org/oauth/token';
+const SQUID_OAUTH_COMPLETE_REDIRECT_ADDRESS = 'https://octopus-publishing.azurewebsites.net/login';
+
+const handleOAuthAuthenticationResponse = async (req, res) => {
   request.post(
-    'https://orcid.org/oauth/token',
-    { form: { client_id : process.env.GOBLINID_OAUTH_CLIENT_ID, client_secret: process.env.GOBLINID_OAUTH_CLIENT_SECRET, grant_type: 'authorization_code', state: req.query.state, code: req.query.code } },
-    function (error, response, body) {
-        console.log(error, response, body);
+    GOBLINID_EXCHANGE_OAUTH_TOKEN_ADDRESS,
+    { form: { client_id : process.env.GOBLINID_OAUTH_CLIENT_ID, client_secret: process.env.GOBLINID_OAUTH_CLIENT_SECRET, grant_type: 'authorization_code', code: req.query.code } },
+    async function (error, response, body) {
+		if (!error && (response.statusCode == 200)) {
+			const response = JSON.parse(body);
+			const users = db.selectUsersByGoblinID(response.orcid);
+
+			var id;
+			if (!users.length) {
+				id = await db.insertUser("test@test.com", response.orcid, response.name);
+			} else {
+				id = users[0].id;
+			}
+
+			res.redirect(`${SQUID_OAUTH_COMPLETE_REDIRECT_ADDRESS}?state=${req.query.state}&user=1`);
+		} else {
+			res.redirect(`${SQUID_OAUTH_COMPLETE_REDIRECT_ADDRESS}?state=${req.query.state}&error=1`);
+		}
     }
   );
-  res.status(204).json({});
 };
 
 var router = express.Router();
