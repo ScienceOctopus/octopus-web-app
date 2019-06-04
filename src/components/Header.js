2a7d3762d0388ca5908740c29d9f74760294234d
Yuriy Maksymets <iurii.maksymets@gmail.com>
2019-06-04 14:58:28 +0100

    First home page mockup

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import "../App.css";
 import WebURI from "../urls/WebsiteURIs";
 import SimpleNav from "./SimpleNav";
 import { LoginDataContext } from "../LoginContext";
+import OctopusLogo from "./OctopusLogo";
 
 const SHOW_SIMPLE_NAV = false;
 
@@ -12,18 +13,13 @@ class Header extends Component {
   static contextType = LoginDataContext;
 
   render() {
-    const loggedIn = (this.context !== undefined);
+    const loggedIn = this.context !== undefined;
 
     return (
       <header className="ui teal inverted menu" style={styles.header}>
         <div className="ui container">
           <Link to={WebURI.Home} className="header item">
-            <img
-              className="logo"
-              src="/images/octopus.png"
-              alt="Octopus Logo"
-              style={styles.logo}
-            />
+            <OctopusLogo style={styles.logo} />
             Octopus
           </Link>
           <Link to={WebURI.Upload} className="item">
@@ -32,29 +28,35 @@ class Header extends Component {
           </Link>
 
           <div className="right item">
-		{ loggedIn ? <div>
-            <img
-              src="/images/avatar.jpg"
-              className="ui avatar image"
-              alt="Avatar of Alex"
-            />
-            <strong>Alex</strong>
-            <div
-              style={styles.logout}
-              className="ui button"
-              /*href="/login?logout=1"*/
-            >
-              Logout
-            </div></div> :
-            <div><a href="https://orcid.org/oauth/authorize?state=1337&client_id=APP-3IQDM9L3ZPD3ZC36&response_type=code&scope=/authenticate&redirect_uri=https://octopus-publishing.azurewebsites.net/api/oauth-flow">
-              <img
-                src="/images/avatar.jpg"
-                className="ui avatar image"
-                alt="Generic avatar"
-                title="Log in"
-              />
-            </a>
-            <span>Log in via ORCiD</span></div> }
+            {loggedIn ? (
+              <div>
+                <img
+                  src="/images/avatar.jpg"
+                  className="ui avatar image"
+                  alt="Avatar of Alex"
+                />
+                <strong>Alex</strong>
+                <div
+                  style={styles.logout}
+                  className="ui button"
+                  /*href="/login?logout=1"*/
+                >
+                  Logout
+                </div>
+              </div>
+            ) : (
+              <div>
+                <a href="https://orcid.org/oauth/authorize?state=1337&client_id=APP-3IQDM9L3ZPD3ZC36&response_type=code&scope=/authenticate&redirect_uri=https://octopus-publishing.azurewebsites.net/api/oauth-flow">
+                  <img
+                    src="/images/avatar.jpg"
+                    className="ui avatar image"
+                    alt="Generic avatar"
+                    title="Log in"
+                  />
+                </a>
+                <span>Log in via ORCiD</span>
+              </div>
+            )}
           </div>
         </div>
 
@@ -67,6 +69,7 @@ class Header extends Component {
 const styles = {
   header: {
     borderRadius: 0,
+    marginBottom: 0,
   },
   logo: {
     marginRight: 1.5 + "em",
--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,8 +1,67 @@
 import React, { Component } from "react";
-import ProblemPage from "./ProblemPage";
+import styled from "styled-components";
+import OctopusLogo from "../components/OctopusLogo";
 
 export default class IndexPage extends Component {
   render() {
-    return <ProblemPage params={{ id: 1 }} publication={false} />;
+    return (
+      <div>
+        <Container>
+          <TitleContainer>
+            <MainTitle>Built for scientists</MainTitle>
+            <SubTitle>
+              Octopus is a publication platform inspired by the way you
+              research. Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit. Esse, doloremque minus. In minima nemo accusantium iste
+              corporis sit ut vero.
+            </SubTitle>
+          </TitleContainer>
+          <OctopusLogo size={600} />
+        </Container>
+
+        <Container height="-">
+          <OctopusLogo size={400} />
+          <TitleContainer>
+            <SubTitle>
+              A lot of very useful information about octopus and the way it
+              works
+            </SubTitle>
+          </TitleContainer>
+        </Container>
+      </div>
+    );
   }
 }
+
+const Container = styled.div`
+  display: flex;
+  padding: 0 12em;
+  background-color: teal;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: ${p => (p.height ? p.height : "80vh")};
+  flex-grow: 1;
+`;
+
+const TitleContainer = styled.div`
+  /* display: flex;
+  flex-direction: column; */
+  flex-grow: 1;
+  flex-basis: 0;
+`;
+
+const MainTitle = styled.div`
+  padding: 3rem 1rem;
+  font-size: 5em;
+  line-height: 1em;
+  font-weight: 500;
+  color: white;
+`;
+
+const SubTitle = styled.div`
+  margin: 1rem 1rem;
+  font-size: 1.4em;
+  line-height: 1.4em;
+  color: lightgray;
+`;
