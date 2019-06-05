42c72ce7759047f23b462e697b1721bd63644d3e
Alexander Harkness <me@bearbin.net>
2019-06-05 10:52:03 +0100

    Add missing source file

---
+++ src/i18n.js
@@ -0,0 +1,38 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    detection: {
+      // order and from where user language should be detected
+      order: ["subdomain", "path", "navigator"],
+
+      // keys or params to lookup language from
+      lookupFromPathIndex: 0,
+      lookupFromSubdomainIndex: 1,
+
+      // cache user language on
+      caches: [],
+    },
+    resources: {
+      en: {
+        translation: {
+          octopus: "Octopus",
+        },
+      },
+      "pt-BR": {
+        translation: {
+          octopus: "jellyblob",
+        },
+      },
+    },
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
