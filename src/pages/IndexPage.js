b084e362a73d014cd6b06b401b220c06ceb07260
Tiger Wang <ziwei.tiger@outlook.com>
2019-06-06 00:17:10 +0100

    Add requisite partner logos to footer

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -2,89 +2,83 @@ import React, { Component } from "react";
 import WebURI from "../urls/WebsiteURIs";
 import { Link } from "react-router-dom";
 import graph from "../assets/images/graph.png";
+import Footer from "../components/Footer";
 
 export default class IndexPage extends Component {
   render() {
     return (
-      <main
-        className="ui middle aligned two column centered stackable grid"
-        style={styles.main}
-      >
-        <div className="eight wide column">
-          <h1 className="ui inverted header" style={styles.heading}>
-            Built for scientists
-          </h1>
-          <h2 style={styles.subtitle}>
-            Octopus is the new way to publish your scientific ideas, findings
-            and research.
-          </h2>
-          <div style={styles.explanation}>
-            Designed to replace journals and papers, Octopus is free to use and
-            gets your work out there much more quickly, to a wider audience and
-            ensures you get maximum credit for the work you do, whether that’s
-            coming up with hypotheses, designing protocols, collecting data,
-            doing analyses or writing reviews.
+      <div>
+        <main
+          className="ui middle aligned two column centered stackable grid"
+          style={styles.main}
+        >
+          <div className="eight wide column">
+            <h1 className="ui inverted header" style={styles.heading}>
+              Built for scientists
+            </h1>
+            <h2 style={styles.subtitle}>
+              Octopus is the new way to publish your scientific ideas, findings
+              and research.
+            </h2>
+            <div style={styles.explanation}>
+              Designed to replace journals and papers, Octopus is free to use
+              and gets your work out there much more quickly, to a wider
+              audience and ensures you get maximum credit for the work you do,
+              whether that’s coming up with hypotheses, designing protocols,
+              collecting data, doing analyses or writing reviews.
+            </div>
+            <div className="ui hidden divider" />
+            <div className="ui stackable grid">
+              <div className="column" style={styles.explore}>
+                <Link
+                  to={WebURI.Explore}
+                  className="ui teal large right labeled icon button"
+                >
+                  <i className="search icon" />
+                  Explore Science
+                </Link>
+              </div>
+              <div className="column" style={styles.login}>
+                <a
+                  className="ui olive large right labeled icon button"
+                  href={WebURI.OrcidLogin(1337)}
+                >
+                  <i className="address book icon" />
+                  Log In via ORCiD
+                </a>
+              </div>
+              <div className="column" style={styles.more}>
+                <Link
+                  to={WebURI.More}
+                  className="ui purple large right labeled icon button"
+                >
+                  <i className="exclamation icon" />
+                  Learn more
+                </Link>
+              </div>
+              <div className="column">
+                <Link
+                  to={WebURI.FAQ}
+                  className="ui violet large right labeled icon button"
+                >
+                  <i className="question icon" />
+                  FAQ
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="ui hidden divider" />
-          <div className="ui stackable grid">
-            <div className="column" style={styles.explore}>
-              <Link
-                to={WebURI.Explore}
-                className="ui teal large right labeled icon button"
-              >
-                <i className="search icon" />
-                Explore Science
-              </Link>
-            </div>
-            <div className="column" style={styles.login}>
-              <a
-                className="ui olive large right labeled icon button"
-                href={WebURI.OrcidLogin(1337)}
-              >
-                <i className="address book icon" />
-                Log In via ORCiD
-              </a>
-            </div>
-            <div className="column" style={styles.more}>
-              <Link
-                to={WebURI.More}
-                className="ui purple large right labeled icon button"
-              >
-                <i className="exclamation icon" />
-                Learn more
-              </Link>
-            </div>
-            <div className="column">
-              <Link
-                to={WebURI.FAQ}
-                className="ui violet large right labeled icon button"
-              >
-                <i className="question icon" />
-                FAQ
-              </Link>
-            </div>
+          <div className="four wide column computer only">
+            <Link to={WebURI.Explore}>
+              <img
+                src={graph}
+                alt="Science Graph Preview"
+                className="ui fluid image"
+              />
+            </Link>
           </div>
-        </div>
-        <div className="four wide column computer only">
-          <Link to={WebURI.Explore}>
-            <img
-              src={graph}
-              alt="Science Graph Preview"
-              className="ui fluid image"
-            />
-          </Link>
-        </div>
-
-        {/* <Container height="30vh">
-          <StyledGraph size={400} />
-          <TitleContainer>
-            <SubTitle>
-              A lot of very useful information about octopus and the way it
-              works
-            </SubTitle>
-          </TitleContainer>
-        </Container> */}
-      </main>
+        </main>
+        <Footer />
+      </div>
     );
   }
 }
