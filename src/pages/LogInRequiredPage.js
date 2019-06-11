c69eb254ec2676b555de0080591bb77b72580eb3
Yuriy Maksymets <iurii.maksymets@gmail.com>
2019-06-11 14:13:00 +0100

    Fixed redirect url on login

--- src/pages/LogInRequiredPage.js
+++ src/pages/LogInRequiredPage.js
@@ -1,21 +1,27 @@
 import React from "react";
-import WebURI, { LocalizedLink } from "../urls/WebsiteURIs";
+import WebURI from "../urls/WebsiteURIs";
+import { withRouter } from "react-router-dom";
 
-const LogInRequiredPage = () => {
+const LogInRequiredPage = withRouter(props => {
   return (
     <div className="ui main text container">
       <div className="ui negative icon message">
         <i className="key icon" />
-        <LocalizedLink to={WebURI.Login}>
+        <a
+          href={WebURI.OrcidLogin(
+            global.session.OAuthState,
+            props.location.pathname,
+          )}
+        >
           <div className="content">
             <div className="header">Log-in required</div>
             <p>Logging in via your ORCiD is required to post publications.</p>
           </div>
-        </LocalizedLink>
+        </a>
       </div>
     </div>
   );
-};
+});
 
 export const loginRequired = Child => props => {
   if (global.session.user === undefined) {
