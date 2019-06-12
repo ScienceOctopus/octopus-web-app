71a4a0c3c9f115441b5eec6484bae4a059f4f2d9
Yuriy Maksymets <iurii.maksymets@gmail.com>
2019-06-12 08:14:55 +0100

    Searching problems in modal

--- src/components/ProblemSelector.js
+++ src/components/ProblemSelector.js
@@ -5,12 +5,33 @@ import SimpleSelector from "./SimpleSelector";
 import Modal from "./Modal";
 import ProblemSearchList from "./ProblemSearchList";
 import GlobalSearch from "./GlobalSearch";
+import SearchField from "./SearchField";
 
 class ProblemSelector extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { query: undefined, modalVisible: true };
   }
+
+  handleSearchChange = event => {
+    this.setState({
+      query: event.target.value,
+    });
+  };
+
+  handleSearchSubmit = event => {
+    this.setState({
+      submittedQuery: this.state.query,
+    });
+
+    event.preventDefault();
+  };
+
+  setModalVisible = visible => () => {
+    this.setState({
+      modalVisible: visible,
+    });
+  };
 
   render() {
     return (
@@ -20,9 +41,17 @@ class ProblemSelector extends Component {
           style={styles.selector}
           {...this.props}
         /> */}
-        <Modal show onClose={() => {}}>
-          <GlobalSearch />
-          <ProblemSearchList />
+        <Modal
+          show={this.state.modalVisible}
+          onClose={this.setModalVisible(false)}
+        >
+          <SearchField
+            placeholder="Search for problems"
+            onChange={this.handleSearchChange}
+            onSubmit={this.handleSearchSubmit}
+            value={this.state.query}
+          />
+          <ProblemSearchList query={this.state.submittedQuery} />
         </Modal>
         {"No suitable problem? "}
         <LocalizedLink
