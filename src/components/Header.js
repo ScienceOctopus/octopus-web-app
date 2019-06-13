ae309ff240dc37e77b33cbf736ca366cd8281258
Moritz Langenstein <ml5717@ic.ac.uk>
2019-06-13 13:21:33 +0100

    (ml5717) Nothing to see here

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,6 @@ import styled from "styled-components";
 
 class Header extends Component {
   render() {
-    global.session.user = { display_name: "Tiger", id: 2, orcid: "muhaha" };
     const loggedIn = global.session.user !== undefined;
 
     return (
