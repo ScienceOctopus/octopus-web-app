e28579a0af61dd2e92a344bb99c88bdde3e79c26
Alexander Harkness <me@bearbin.net>
2019-06-17 14:17:22 +0100

    Fix broken review fetching for logged in users

--- server/postgresQueries.js
+++ server/postgresQueries.js
@@ -205,7 +205,7 @@ const queries = {
 
   selectAllReviewPublicationsByPublication: publication =>
     queries
-      .selectCompletedPublicationsByLinksBeforePublication(publication)
+      .selectAllPublicationsByLinksBeforePublication(publication)
       .where("review", true),
 
   selectCompletedReviewPublicationsByPublication: publication =>
