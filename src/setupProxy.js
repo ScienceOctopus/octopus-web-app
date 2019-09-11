9c318f6771348f3578595c7bcbd98de518e8d0df
Vot <vot@siliconfen.co>
2019-09-11 23:36:01 +0100

    Change changeOrigin to false

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use('/api', proxy({
     target: 'http://localhost:3001/',
-    changeOrigin: true,
+    changeOrigin: false,
     ws: true,
   }));
 };
